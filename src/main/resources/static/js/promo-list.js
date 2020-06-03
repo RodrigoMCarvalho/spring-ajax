0c969b7d718c5c6d26676a68dadee30d09dbd799
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-06-03 20:59:25 -0300

    Listando as promoções com AJAX

---
+++ src/main/resources/static/js/promo-list.js
@@ -0,0 +1,54 @@
+
+var pageNumber = 0;
+
+$(document).ready(function() {
+    $("#loader-img").hide();
+    $("#fim-btn").hide();
+});
+
+//efeito infinite scroll
+$(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    var conteudo = $(document).height() - $(window).height();
+    //console.log("scrolltop: ", scrollTop, " | ", "conteudo: ", conteudo);
+
+    if (scrollTop >= conteudo) {
+        //console.log("****");
+        pageNumber++;
+        setTimeout(function() {
+            loadByScrollBar(pageNumber);
+        }, 200);
+    }
+});
+
+function loadByScrollBar(pageNumber) {
+    $.ajax({
+        method: "GET",
+        url: "/promocao/list/ajax",
+        data: {
+            page: pageNumber
+        },
+        beforeSend: function() {
+            $("#loader-img").show();
+        },
+        success: function(response) {
+            //console.log("response: ", response);
+            console.log("lista: ", response.length);
+
+            if(response.length > 150) {
+                $(".row").fadeIn(250, function() {
+                    $(this).append(response);
+                });
+            } else {
+                $("#fim-btn").show();
+                $("#loader-img").removeClass("loader");
+            }
+        },
+        error: function(xhr) {
+            alert("Ops, ocorreu um erro: ", xhr.status + xhr.statusText);
+        },
+        complete: function() {
+            $("#loader-img").hide();
+        }
+    })
+}
